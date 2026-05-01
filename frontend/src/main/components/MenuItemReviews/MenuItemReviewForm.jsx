b3ce96b32c53bd9a708cf7eae65a8d0d4b39a3f6
Elissa Alawi <elissaalawi@ucsb.edu>
2026-05-01 16:02:34 -0700

    Added more tests for mutation coverage

--- frontend/src/main/components/MenuItemReviews/MenuItemReviewForm.jsx
+++ frontend/src/main/components/MenuItemReviews/MenuItemReviewForm.jsx
@@ -141,7 +141,7 @@ function MenuItemReviewForm({
         </Form.Control.Feedback>
       </Form.Group>
 
-      <Button type="submit" data-testid={testIdPrefix + "-submit"}>
+      <Button type="submit">
         {buttonLabel}
       </Button>
       <Button
