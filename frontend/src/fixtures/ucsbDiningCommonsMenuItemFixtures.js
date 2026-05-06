8e0dbb7d9629649b98643a039520e9029e05aa2a
jdelpego <jdelpego@ucsb.edu>
2026-05-05 17:02:07 -0700

    Add fixtures for UCSBDiningCommonsMenuItem

---
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -0,0 +1,35 @@
+const ucsbDiningCommonsMenuItemFixtures = {
+  oneMenuItem: [
+    {
+      id: 1,
+      diningCommonsCode: "ortega",
+      name: "Baked Pesto Pasta with Chicken",
+      station: "Entree Specials",
+    },
+  ],
+
+  threeMenuItems: [
+    {
+      id: 2,
+      diningCommonsCode: "ortega",
+      name: "Tofu Banh Mi Sandwich (v)",
+      station: "Entree Specials",
+    },
+
+    {
+      id: 3,
+      diningCommonsCode: "portola",
+      name: "Cream of Broccoli Soup (v)",
+      station: "Greens & Grains",
+    },
+
+    {
+      id: 4,
+      diningCommonsCode: "de-la-guerra",
+      name: "Chicken Caesar Salad",
+      station: "Entrees",
+    },
+  ],
+};
+
+export { ucsbDiningCommonsMenuItemFixtures };
